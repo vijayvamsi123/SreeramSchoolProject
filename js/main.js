3e3c58988f07a4c8c70420a562ceb2cc78750ac7
vijayvamsi123 <nvijayvamsi@gmail.com>
2023-03-11 11:11:45 +0530

    verson2.1

--- js/main.js
+++ js/main.js
@@ -56,3 +56,50 @@
     
 })(jQuery);
 
+const btns = document.querySelectorAll('.btn-g');
+const SchoolGallerys = document.querySelectorAll('.School-Gallery');
+// const search = document.getElementById(search);
+
+for (i = 0; i < btns.length; i++) { 
+
+    btns[i].addEventListener('click', (e) => {
+        e.preventDefault()
+
+        const filter = e.target.dataset.filter;
+        console.log(filter);
+
+        SchoolGallerys.forEach((image) => {
+            if (filter === 'all') {
+                image.style.display = 'block'
+            } else {
+                if (image.classList.contains(filter)) {
+                    image.style.display = 'block'
+                } else {
+                    image.style.display = 'none'
+                }
+            }
+        });
+    });
+};
+
+
+// SEARCH FILTER
+const search = document.getElementById("search");
+const productName = document.querySelectorAll(".product-details h2");
+
+// A BETTER WAY TO FILTER THROUGH THE PRODUCTS
+search.addEventListener("keyup", filterProducts);
+
+
+function filterProducts(e) {
+    const text = e.target.value.toLowerCase();
+    // console.log(productName[0]);
+    productName.forEach(function(product) {
+        const item = product.firstChild.textContent;
+        if (item.toLowerCase().indexOf(text) != -1) {
+            product.parentElement.parentElement.style.display = "block"
+        } else {
+            product.parentElement.parentElement.style.display = "none"
+        }
+    })
+}
